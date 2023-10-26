f5dc66f15144b196de457101b9c7c9fc2c5e832f
Abdul Azis <abdulazisnaufal@gmail.com>
2023-10-27 00:50:02 +0700

    update app.go

--- Donor_Darah/app.go
+++ Donor_Darah/app.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"app/config"
+	"app/models"
 	"app/routes"
+	"reflect"
 )
 
 func main() {
@@ -10,6 +12,21 @@ func main() {
 	config.ConnectDB()
 
 	e := routes.Init()
+
+	models := []interface{}{
+		reflect.TypeOf(models.Admin{}),
+		reflect.TypeOf(models.DaftarDonor{}),
+		reflect.TypeOf(models.Gol_Darah{}),
+		reflect.TypeOf(models.Jadwal{}),
+		reflect.TypeOf(models.Order{}),
+		reflect.TypeOf(models.Pusat{}),
+		reflect.TypeOf(models.Stok{}),
+		reflect.TypeOf(models.User{}),
+	}
+	
+	for _, m := range models {
+		config.DB.AutoMigrate(m)
+	}
 
 	e.Logger.Fatal(e.Start(":8000"))
 }
