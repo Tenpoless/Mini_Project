7713afb97114309aa12e4536f8c5b379a78e0ad8
Abdul Azis <abdulazisnaufal@gmail.com>
2023-10-30 05:17:53 +0700

    .

--- config/database.go
+++ config/database.go
@@ -4,21 +4,21 @@ import (
 	"fmt"
 	"os"
 
-	// "path/filepath"
+	"path/filepath"
 
-	// "github.com/joho/godotenv"
-	// "gorm.io/driver/mysql"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func ConnectDB() {
-	// err := godotenv.Load(filepath.Join(".", ".env"))
-	// if err != nil {
-	// 	fmt.Println("Error loading .env file")
-	// 	os.Exit(1)
-	// }
+	err := godotenv.Load(filepath.Join(".", ".env"))
+	if err != nil {
+		fmt.Println("Error loading .env file")
+		os.Exit(1)
+	}
 
 	dbUser := os.Getenv("DB_USER")
 	dbPass := os.Getenv("DB_PASS")
@@ -29,11 +29,11 @@ func ConnectDB() {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		dbUser, dbPass, dbHost, dbPort, dbName)
 
-	// var errDB error
-	// DB, errDB = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	// if errDB != nil {
-	// 	panic("Failed to Connect Database")
-	// }
+	var errDB error
+	DB, errDB = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if errDB != nil {
+		panic("Failed to Connect Database")
+	}
 
 	fmt.Println("Connected to Database")
 }
