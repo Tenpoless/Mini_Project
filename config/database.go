fffd85702193d847012945bb9ea5606314af309e
Abdul Azis <abdulazisnaufal@gmail.com>
2023-10-30 10:26:56 +0700

    .

--- config/database.go
+++ config/database.go
@@ -1,31 +1,31 @@
 package config
 
 import (
-	"fmt"
+
 	"os"
 
 
-	"gorm.io/driver/mysql"
+
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func ConnectDB() {
-	dbUser := os.Getenv("DB_USER")
-	dbPass := os.Getenv("DB_PASS")
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbName := os.Getenv("DB_NAME")
+	os.Getenv("DB_USER")
+	os.Getenv("DB_PASS")
+	os.Getenv("DB_HOST")
+	os.Getenv("DB_PORT")
+	os.Getenv("DB_NAME")
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbPort, dbName)
+	// dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbPort, dbName)
 
-	var errDB error
-	DB, errDB = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if errDB != nil {
-		panic("Gagal terhubung ke database")
-	}
+	// var errDB error
+	// DB, errDB = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// if errDB != nil {
+	// 	panic("Gagal terhubung ke database")
+	// }
 
-	fmt.Println("Terhubung ke Database")
+	// fmt.Println("Terhubung ke Database")
 }
 
