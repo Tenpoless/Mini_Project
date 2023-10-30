7ab26a0b0192e28534444e4f0295e58b93539010
Abdul Azis <abdulazisnaufal@gmail.com>
2023-10-30 10:20:50 +0700

    .

--- config/database.go
+++ config/database.go
@@ -4,19 +4,28 @@ import (
 	"fmt"
 	"os"
 
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func ConnectDB() {
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
 
-	os.Getenv("DB_USER")
-	os.Getenv("DB_PASS")
-	os.Getenv("DB_HOST")
-	os.Getenv("DB_PORT")
-	os.Getenv("DB_NAME")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbUser, dbPass, dbHost, dbPort, dbName)
 
-	fmt.Println("Connected to Database")
+	var errDB error
+	DB, errDB = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if errDB != nil {
+		panic("Gagal terhubung ke database")
+	}
+
+	fmt.Println("Terhubung ke Database")
 }
 
